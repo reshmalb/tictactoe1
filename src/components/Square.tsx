62288d04da31ae8f2fb77436af78811afeb085a6
reshmalb <reshma.lb27@gmail.com>
2023-06-14 12:20:23 +0530

    final

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,12 +1,25 @@
+import { NumberLiteralType } from "typescript"
+
 interface SquareProps{
     onClick:()=>void,
-    value:'X'|'O'|null
+    value:'X'|'O'|null,
+    winner:string|null
 }
 
-export const Square:React.FC<SquareProps>=({onClick,value})=>{
+export const Square:React.FC<SquareProps>=({onClick,value,winner})=>{
+  if(!value){
     return(
-    <button className='gameboard-square'
-        onClick={onClick}
+        <button className={`gameboard-square player_${value}`}
+            onClick={onClick}
+            disabled={Boolean(winner)}
+        >{value}
+        
+        </button>
+        )
+  }
+  return(
+    <button className={`gameboard-square player_${value}`}
+        disabled
     >{value}
     
     </button>
--- src/pages/GameBoard.tsx
+++ src/pages/GameBoard.tsx
@@ -1,12 +1,13 @@
 import {Grid}from 'semantic-ui-react'
 import { Square } from '../components/Square'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+type player='X'|'O'|null;
 export const GameBoard:React.FunctionComponent = () => {
     
      const activeplayer=Math.round(Math.random()+1)===1 ?'X':'O';
      const [currentPlayer,setCurrentPlayer]=useState<'X'|'O'>(activeplayer);
-     const [squares,setSquares]=useState(Array(9).fill(null));
-
+     const [squares,setSquares]=useState<player[]>(Array(9).fill(null));
+        const [winner,setWinner]=useState<string | null>(null);
    const setSquareValue=(index:number)=>{
       const currentGameStatus= squares.map((value,i)=>{
         if(i===index){
@@ -16,16 +17,43 @@ export const GameBoard:React.FunctionComponent = () => {
         return value;
     })
     setSquares(currentGameStatus);
-    setCurrentPlayer(currentPlayer==='X'? 'O':'X')
+    setCurrentPlayer(currentPlayer==='X'? 'O':'X')     
 
+   }
+   useEffect(()=>{
+   const winnerplayer=calculateWinner(squares)
+   if(winnerplayer){
+    setWinner(`${winnerplayer} is the winner`)
+   }else if(!winnerplayer && !squares.filter((square)=>!square).length){
+      setWinner(`Both players won.Reload for new game..`)
+   }
+       
+   })
+   const  calculateWinner= (squares:player[])=>{
+    const winnerIndexCombinations=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+   return winnerIndexCombinations.map((combinations)=>{
+        const [a,b,c]=combinations;
+        if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){
+            return squares[a]
+        }
+    }).filter((winner)=>winner)[0]
 
-      
 
    }
 
   return (
     <div  className='container-gameboard'>
-        <p>hey{currentPlayer} it's yuour turn</p>
+        {winner ? <p>{winner}</p>:<p>Hey,{currentPlayer} it's your turn..</p>}
+       
         <Grid columns={3} centered> 
             {
                 Array(9).fill(null).map((value,index)=>(
@@ -33,6 +61,7 @@ export const GameBoard:React.FunctionComponent = () => {
                    <Square
                    onClick={()=>setSquareValue(index)}
                    value={squares[index]}
+                   winner={winner}
                      />
                 </Grid.Column>))
             }
